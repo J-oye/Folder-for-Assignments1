a1613c8030c2f23c360ebc8a242b0d4473e20d47
JOSHUA-OYELEKE <joshoyeleke@gmail.com>
2023-04-07 14:03:48 +0100

    javaprojects

---
+++ src/deitel/chapter2/Arithmetic.java
@@ -0,0 +1,2 @@
+package deitel.chapter2;public class Arithmetic {
+}
---
+++ src/tdd/Pyramid.java
@@ -0,0 +1,63 @@
+package tdd;
+
+import java.util.Scanner;
+
+/**public class Numbers{
+    public static void main(String[] args) {
+      /**  Scanner input= new Scanner(System.in);
+     //   System.out.println("Enter a number");
+        int num = 10;
+      int counter=1;
+      while(counter<=num){
+          if(counter%2!=num)
+          System.out.println(counter);
+          counter +=2;
+    }*/
+     /** Scanner input = new Scanner(System.in);
+      int sales;
+      int totalSales = 0;
+      int counter =0;
+      while(counter<5) {
+          System.out.println("Enter sale for product" + (counter));
+          sales = input.nextInt();
+          totalSales += sales;
+          counter = counter + 1;
+          System.out.println("Total sale is now " + totalSales);
+      }
+}
+}*/
+
+    /** public  class SentinelLoop{
+         public static void main(String[] args){
+             Scanner input = new Scanner(System.in);
+             System.out.println("Enter sale price for your product or-1 to stop");
+             int userInput = input.nextInt();
+             int totalSales=0;
+             while(userInput != -1){
+                 totalSales += userInput;
+                 System.out.println("Enter sale price for your product or -1 to stop");
+                 userInput = input.nextInt();
+             }
+             System.out.println("Total sales is" +totalSales);
+         }
+     }*/
+
+  /**  public class ForLoop{
+        public static void main(String[] args){
+            for(int i =7;i<=77;i+=7)
+                System.out.println(i);
+
+        }
+    }*/
+
+  public class Pyrimid{
+      public static void main(String[] args){
+          int sum = 7;
+          int j = 0;
+          while(j<=7)
+              System.out.println("*");
+          j+=2;
+
+          }
+      }
+  //}
